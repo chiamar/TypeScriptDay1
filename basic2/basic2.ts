fb8f69a018f4bf5d68050b49544265fcdd6c700a
Petra Christina <p.sedlaczek@drei.at>
2022-02-10 12:38:36 +0100

    Basic exercise 2

---
+++ basic2/basic2.ts
@@ -0,0 +1,30 @@
+// let myName: Array <{fName: string, lName: string}> = [{
+let myName: {fName: string, lName: string}[] = [{
+  fName: "Petra",
+  lName: "Sedlaczek",
+}]
+
+let body = document.body
+body.innerHTML = `${myName[0].fName} ${myName[0].lName}<hr>`
+
+myName.forEach(function(val) {
+  for (let i=0; i<10; i++) {
+    body.innerHTML += val.fName + "<br>";
+  }
+  body.innerHTML += "<hr>"
+});
+
+setTimeout(() => {
+  myName.forEach(function(val) {
+    for (let i=0; i<10; i++) {
+      console.log(val.lName)
+    }
+  })}, 5000);
+
+setTimeout(() => {
+  myName.forEach(function(val) {
+    for (let i=0; i<10; i++) {
+      body.innerHTML += val.lName + "<br>"
+    }
+  })}, 5000);
+
